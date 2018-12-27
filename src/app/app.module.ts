b69dfd168e0a1070da1321d893cfcdd0e6ca19c8
julissamackeydev <julissamackey@Julissas-MacBook-Air.local>
2018-12-27 17:16:31 -0500

    created some more components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { BioComponent } from './bio/bio.component';
+import { EngagementsComponent } from './engagements/engagements.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    BioComponent,
+    EngagementsComponent
   ],
   imports: [
     BrowserModule,
